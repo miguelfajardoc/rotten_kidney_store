94ab651848c748becc7e6d4aeab0569193a3c41e
miguelfajardoc <miky116@gmail.com>
2026-08-27 17:26:16 -0500

    add images for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,6 +65,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.expect(product: [ :stock, :price, :cost, :aditional_info, :character_id, :product_kind_id ])
+      params.expect(product: [ :stock, :price, :cost, :aditional_info, :character_id, :product_kind_id, images: [] ])
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :character
   belongs_to :product_kind
+
+  has_many_attached :images
 end
